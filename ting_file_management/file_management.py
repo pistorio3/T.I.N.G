0ef57c667f76c6dfe004f66d6ff3e1310b223c12
João Victor Pistorio <joao.pistorio@gmail.com>
2022-02-21 11:35:01 -0300

    requirement 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,14 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    try:
+        bool(path_file.split('.txt', 1)[1])
+    except Exception:
+        sys.stderr.write('Formato inválido\n')
+    try:
+        with open(path_file) as file:
+            content = file.read()
+            return content.splitlines
+    except FileNotFoundError:
+        return sys.stderr.write(f'Arquivo {path_file} não encontrado\n')
