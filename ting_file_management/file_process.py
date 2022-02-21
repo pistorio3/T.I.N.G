204fd969b6f8c72598f2f8d80b48904581d28df0
João Victor Pistorio <joao.pistorio@gmail.com>
2022-02-21 12:26:32 -0300

    requirement 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,22 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    for file in range(len(instance.data)):
+        if path_file == instance.search(file)['nome_do_arquivo']:
+            return None
+
+    content = txt_importer(path_file)
+
+    result = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(content),
+        "linhas_do_arquivo": content,
+    }
+
+    instance.enqueue(result)
+    sys.stdout.write(str(result))
 
 
 def remove(instance):
