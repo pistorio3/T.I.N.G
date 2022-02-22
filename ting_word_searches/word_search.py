bfc925d82d25d25e0ebf114810bc421b063bba41
João Victor Pistorio <joao.pistorio@gmail.com>
2022-02-21 23:36:33 -0300

    requirement 6 and 7

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,46 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    words_list = []
+
+    for file in instance.data:
+        occurrences = []
+
+        for index, line in enumerate(file["linhas_do_arquivo"]):
+            if word.lower() in line.lower():
+                info = ({"linha": index + 1})
+            else:
+                return words_list
+            occurrences.append(info)
+
+        if occurrences:
+            words_list.append(
+                {
+                    "palavra": word,
+                    "arquivo": file["nome_do_arquivo"],
+                    "ocorrencias": occurrences,
+                }
+            )
+    return words_list
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    words_list = []
+
+    for file in instance.data:
+        occurrences = []
+
+        for index, line in enumerate(file["linhas_do_arquivo"]):
+            if word.lower() in line.lower():
+                info = ({"linha": index + 1, "conteudo": line})
+            else:
+                return words_list
+            occurrences.append(info)
+
+        if occurrences:
+            words_list.append(
+                {
+                    "palavra": word,
+                    "arquivo": file["nome_do_arquivo"],
+                    "ocorrencias": occurrences,
+                }
+            )
+    return words_list
